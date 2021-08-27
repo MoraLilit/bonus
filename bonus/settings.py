7e00f2cf3f7d70d439a59a0c354d51a7144dceca
Mora <mora.lilit@icloud.com>
2021-08-27 05:22:51 +0300

    Settings

--- bonus/settings.py
+++ bonus/settings.py
@@ -108,22 +108,22 @@ SETTINGS_APPLICATION = 'settings.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASE_URL = os.environ['postgres://kaxajybwrfedpq:334bc277eb784ecb707b2f6895c678371ae485e7e5719945f3cc1150ee054f21@ec2-79-125-30-28.eu-west-1.compute.amazonaws.com:5432/d940nub77vcl40']
+'''DATABASE_URL = os.environ['//kaxajybwrfedpq:334bc277eb784ecb707b2f6895c678371ae485e7e5719945f3cc1150ee054f21@ec2-79-125-30-28.eu-west-1.compute.amazonaws.com:5432/d940nub77vcl40']
 
 conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)'''
 
-'''DATABASES = {
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'bonuses',
         'USER': 'postgres',
         'PASSWORD': '1532',
-        'HOST': 'localhost',
+        'HOST': '.herokuapp.com',
         'PORT': '5432',
     }
-}'''
+}
 
 AUTHENTICATION_BACKENDS = [
     # Needed to login by username in Django admin, regardless of `allauth`
