d81845f331b275f59e4d41cac461fc765332c664
Mora <mora.lilit@icloud.com>
2021-08-27 14:11:55 +0300

    Settings

--- bonus/settings.py
+++ bonus/settings.py
@@ -187,7 +187,7 @@ STATIC_ROOT = BASE_DIR / 'letstart/static'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
-# STATIC_URL = '/static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
